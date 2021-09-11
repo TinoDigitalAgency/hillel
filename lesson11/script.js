653bcc9da01cd04addba1b89165639a27cf2d26c
Tino <tino.agency@gmail.com>
2021-09-11 16:11:45 +0300

    ДЗ 11

--- lesson11/script.js
+++ lesson11/script.js
@@ -3,8 +3,9 @@ const button = document.getElementById('add-task');
 const todos = document.getElementsByClassName('todo-item');
 const todoWrapper = document.getElementsByClassName('todo-wrapper')[0];
 let counter = 0;
+
 const checkToDo = (items) => {
-    if(items.length > 0) {
+    if (items.length > 0) {
         // show todos
     } else {
         todoWrapper.innerHTML = "<p class='no-task-text'>You don't have any scheduled tasks yet</p>";
@@ -13,14 +14,14 @@ const checkToDo = (items) => {
 
 const addTask = () => {
     const inputValue = input.value;
-    if(inputValue !== '') {
+    if (inputValue !== '') {
         const id = 'todo-item-' + Math.random() * 100 * counter;
-        if(todoWrapper.getElementsByClassName('no-task-text').length > 0) {
+        if (todoWrapper.getElementsByClassName('no-task-text').length > 0) {
             todoWrapper.getElementsByClassName('no-task-text')[0].remove();
         }
 
         todoWrapper.innerHTML += "<div class='todo-item' id='" + id + "' onclick='completeTask(id)'>" +
-            "<div class='todo-item-content'>"+inputValue+"</div>" +
+            "<div class='todo-item-content'>" + inputValue + "</div>" +
             "</div>"
 
         counter += 1;
@@ -31,6 +32,7 @@ const addTask = () => {
         return false;
     }
 }
+
 const completeTask = (element) => {
     if (element !== null && element.length > 0) {
         const elementObj = document.getElementById(element);
@@ -40,14 +42,12 @@ const completeTask = (element) => {
             elementObj.classList.add('completed');
         }
 
-    } else  {
+    } else {
         return false
     }
 }
 
-
 checkToDo(todos);
-
 button.addEventListener('click', addTask);
 
 
